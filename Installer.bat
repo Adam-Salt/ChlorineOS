8dc9605fb4663a7f83b08f5536038fee4cb91de6
Lightning3240 <Lightning3240@outlook.com>
2019-11-16 08:20:33 +0000

    Add files via upload

--- Installer.bat
+++ Installer.bat
@@ -5,5 +5,5 @@ color 0b
 cls
 echo.
 echo Getting Latest Version
-curl -o ChlorineOS.bat "https://raw.githubusercontent.com/Lightning3240/ChlorineOS/master/ChlorineOS.bat?_=%random%" -L
+curl -o Chlorine.bat "https://raw.githubusercontent.com/Lightning3240/ChlorineOS/master/Chlorine.bat?_=%random%" -L
 EXIT
